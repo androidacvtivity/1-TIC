58ee519ad85777f4ba01ca1815d084325d92bbd8
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2024-02-06 08:55:53 +0200

    Cap 2

--- 1182_2012.js
+++ 1182_2012.js
@@ -5,7 +5,7 @@ this.ChangeCheckbox_1182 = function (itemId) {
     var isChecked = document.getElementById(itemId).checked;
     
     //Cap 2
-    //Start row 210
+   
     if (itemId == "71_1182_77841_210_1" && isChecked == true) {
         document.getElementById("71_1182_77841_210_2").checked = false;
     }
@@ -13,7 +13,55 @@ this.ChangeCheckbox_1182 = function (itemId) {
         document.getElementById("71_1182_77841_210_1").checked = false;
 
     }
-    //End row 210
+  
+
+
+
+    else  if (itemId == "71_1182_77840_221_1" && isChecked == true) {
+        document.getElementById("71_1182_77840_221_2").checked = false;
+    }
+    else if (itemId == "71_1182_77840_221_2" && isChecked == true) {
+        document.getElementById("71_1182_77840_221_1").checked = false;
+
+    }
+
+    else if (itemId == "71_1182_77837_222_1" && isChecked == true) {
+        document.getElementById("71_1182_77837_222_2").checked = false;
+    }
+    else if (itemId == "71_1182_77837_222_2" && isChecked == true) {
+        document.getElementById("71_1182_77837_222_1").checked = false;
+
+    }
+
+
+    else if (itemId == "71_1182_77844_223_1" && isChecked == true) {
+        document.getElementById("71_1182_77844_223_2").checked = false;
+    }
+    else if (itemId == "71_1182_77844_223_2" && isChecked == true) {
+        document.getElementById("71_1182_77844_223_1").checked = false;
+
+    }
+
+
+ 
+
+
+    else if (itemId == "71_1182_77845_224_1" && isChecked == true) {
+        document.getElementById("71_1182_77845_224_2").checked = false;
+    }
+    else if (itemId == "71_1182_77845_224_2" && isChecked == true) {
+        document.getElementById("71_1182_77845_224_1").checked = false;
+
+    }
+
+    else if (itemId == "71_1182_77846_230_1" && isChecked == true) {
+        document.getElementById("71_1182_77846_230_2").checked = false;
+    }
+    else if (itemId == "71_1182_77846_230_2" && isChecked == true) {
+        document.getElementById("71_1182_77846_230_1").checked = false;
+
+    }
+
 
 
 }
